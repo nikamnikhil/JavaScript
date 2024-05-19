610292f6029120fc2bfc6db0ab22ce7739d5b11d
nikhil nikam <106835674+nikamnikhil@users.noreply.github.com>
2024-05-19 10:06:35 +0000

    operations in js

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,42 @@
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(5%3);
+
+let str1="hello";
+let str2=" Nikhil";
+let str3=str1+str2; // only additions is allowed no substraction
+console.log(str3);
+
+//before stirng it take number as a number and do arithmatic opearations
+// after string it takes number as a string 
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+"2"+2+2); 
+// console.log(1+2+2+"2");
+// console.log(1+2+"2"+2+3*4+2);
+
+console.log(+true)
+console.log(+"")
+
+// prefix increament postfix increment
+let number=100;
+console.log(number)
+++number;
+console.log(number);
+number++;
+console.log(number);
+
+// prefix decrement postfix decrement
+let number2=100;
+console.log(number2)
+--number2;
+console.log(number2);
+number2--;
+console.log(number2);
