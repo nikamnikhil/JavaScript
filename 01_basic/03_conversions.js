31b2d630e2dee44a095cb0c75584b1583b50e663
nikhil nikam <106835674+nikamnikhil@users.noreply.github.com>
2024-05-19 09:30:01 +0000

    conversion of values

---
+++ 01_basic/03_conversions.js
@@ -0,0 +1,49 @@
+let points=45;
+
+// console.log(typeof points); // number
+let valueInNumber= Number(points); 
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //number
+
+let score="33abc";
+
+// console.log(typeof score);
+// console.log(typeof score);
+let valueInNumber2= Number(score);
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+let isLoggedIn=1;
+
+// console.log(typeof isLoggedIn);
+let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean)
+
+let state=null;
+// console.log(typeof state); // object
+let valueInNumber3= Number(state);
+// console.log(typeof valueInNumber3); // number
+// console.log(valueInNumber3); // 0
+let valueInString2=String(state);
+// console.log(typeof valueInString2); // string
+// console.log(valueInString2); // null
+
+let valueInBoolean2=Boolean(state);
+// console.log(typeof valueInBoolean2); // boolean
+// console.log(valueInBoolean2); // false
+
+let name=undefined;
+
+console.log(typeof name);
+let valueInNumber4=Number(name);
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+
+let valueInString3=String(name);
+console.log(typeof valueInString3)
+console.log(valueInString3)
+
+let valueInBoolean3=Boolean(name);
+console.log(typeof valueInBoolean3)
+console.log(valueInBoolean3)
