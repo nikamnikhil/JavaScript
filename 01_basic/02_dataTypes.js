2de961e0a9cb3e7c90addb01287672a3f76d8b1a
nikhil nikam <106835674+nikamnikhil@users.noreply.github.com>
2024-05-19 08:44:51 +0000

    learning datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //we are using node js , not browser
+
+console.log(3
+    + 3
+);  // follow best practices, Code readability should be high
+
+console.log("Hello world!");
+
+let name="nikhil";
+let age=26;
+let isLoggedIn=false;
+let state;
+
+//string => "" or ''
+//number => 2 to power 53
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  //undefined
