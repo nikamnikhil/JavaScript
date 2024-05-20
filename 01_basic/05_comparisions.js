6dcc80a7208b1f3e54f08738413802a870c180c4
nikhil nikam <106835674+nikamnikhil@users.noreply.github.com>
2024-05-20 19:10:16 +0000

    comparisions concepts

---
+++ 01_basic/05_comparisions.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=2);
+// console.log(2<2);
+// console.log(2<1);
+
+// console.log("2">1); // string converts to number
+// console.log("2">=2);
+// console.log("2">=3);
+
+// console.log(null>0);
+// console.log(null==null);
+// console.log(null>=0);
+// console.log(null<0);
+// console.log(null<=0);
+// console.log(null===0);
+
+// console.log(undefined>0);
+// console.log(null==undefined);
+// console.log(undefined>=0);
+// console.log(undefined<0);
+// console.log(undefined<=0);
+// console.log(undefined==0);
+
+//use strict check === (checks data types and value)
+console.log("2"==2);
+console.log("2"===2);
+console.log(true==1);
+console.log(true===1);
